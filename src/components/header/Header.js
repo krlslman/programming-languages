5fd47ae7b2331bfd0c38f26290734cacf2e184ab
krlslman <selmankorall@gmail.com>
2023-02-24 22:00:38 +0300

    Info div shows up on hover on question mark

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,11 +1,36 @@
 import './Header.css'
 import logo from '../../logo.svg'
+import { useState } from 'react';
 
 const Header = () => {
+    const [showInfo, setShowInfo] = useState(false);
+
+    const handleMouseEnter = () => {
+        setShowInfo(true);
+    };
+
+    const handleMouseLeave = () => {
+        setShowInfo(false);
+    };
+    
     return (
         <div className='logo-container'>
             <img className='header-logo' src={logo} alt="logo" />
             <h2>Language Card Project!</h2>
+            <img className='question-mark' src="https://cdn-icons-png.flaticon.com/512/5726/5726716.png" 
+                alt='question-mark' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} />
+            {/* <div className='info'>
+                This is the info on hover...
+            </div> */}
+
+            {showInfo && (
+                <div className="info">
+                    <p>This info block shows up on hover on Qmark.</p>
+                    <p>You can click on the cards to see more about it.</p>
+                    <p>After 2 seconds, card will flip back automatically.</p>
+                </div>
+            )}
+
         </div>
     )
 }

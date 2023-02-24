598b8e5857c5981e0c2263d366dc2cb9a9456e53
krlslman <selmankorall@gmail.com>
2023-02-24 21:03:19 +0300

    Cards are flipping back after 2 seconds - setTimout

--- src/components/item/Item.js
+++ src/components/item/Item.js
@@ -1,15 +1,30 @@
 import './Item.css'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const Item = ({card}) => {
     const [showLogo, setShowLogo] = useState(true)
     const {name, img, options} = card;
 
+    // this part flips the card on click
+    const handleCardClick = () => {
+        setShowLogo(!showLogo);
+    }
+    // this part works after any update.
+    useEffect(() => {
+        if (!showLogo) {
+            const flipTimeout = setTimeout(() => {
+                setShowLogo(true);
+            }, 2000);
+
+            return () => clearTimeout(flipTimeout);
+        }
+    }, [showLogo]);
+
     return (
-        <div className="card" onClick={() => setShowLogo(!showLogo)}>
+        <div className="card" onClick={handleCardClick}>
             {showLogo ? 
                 (<div>
-                    <img className="card-logo" src={img} ></img>
+                    <img className="card-logo" src={img} alt="logo"/>
                     <h3 className="card-title">{name}</h3>
                 </div>)
                 : 
